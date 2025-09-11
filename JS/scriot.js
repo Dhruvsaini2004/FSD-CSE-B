50342069ce8e1c38be0d756f86e58319a6e86d5e
Dhruv Saini <dhruvsaini0125@gmail.com>
2025-09-11 15:33:23 +0530

    script.js added

---
+++ JS/scriot.js
@@ -0,0 +1,44 @@
+function waitfortwoseconds()
+{
+    const ms=new Date().getTime()+2000;
+    while(new Date().getTime()<ms);
+}
+function register()
+{
+    waitfortwoseconds();
+    console.log("register end");
+}
+function sendemail()
+{
+    waitfortwoseconds();
+    console.log("email end");
+}
+function login()
+{
+    waitfortwoseconds();
+    console.log("login end");
+}
+function getData()
+{
+    waitfortwoseconds();
+    console.log("data end");
+}
+function displayData()
+{
+    waitfortwoseconds();
+    console.log("Display end");
+}
+register();
+sendemail();
+login();
+getData();
+displayData();
+console.log("other Application")
+
+
+const wait = ()=>{
+    setTimeout(() => {
+        console.log('Hello');
+    }, 2000);
+}
+wait();
